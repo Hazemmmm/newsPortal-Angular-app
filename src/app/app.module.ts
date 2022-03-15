4fbc6d738c4c3fa9a5f1135bc7a94501c5e67679
Hazem <hazemzon@gmail.com>
2022-03-15 15:28:15 +0200

    add tabView [meubar] component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleCardComponent } from './modules/cards/article-card/article-card.component';
 import { NavBarComponent } from './modules/navBar/nav-bar/nav-bar.component';
 import { PrimengSharedModule } from './shared/primeng-shared/primeng-shared.module';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent],
+  declarations: [AppComponent, NavBarComponent, ArticleCardComponent],
   imports: [BrowserModule, AppRoutingModule, PrimengSharedModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/shared/primeng-shared/primeng-shared.module.ts
+++ src/app/shared/primeng-shared/primeng-shared.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenubarModule } from 'primeng/menubar';
 // import { MenuItem } from 'primeng/api';
+import { InputTextModule } from 'primeng/inputtext';
+import { TabViewModule } from 'primeng/tabview';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
-  imports: [CommonModule, MenubarModule],
-  exports: [MenubarModule],
+  imports: [
+    CommonModule,
+    MenubarModule,
+    InputTextModule,
+    TabViewModule,
+    ButtonModule,
+  ],
+  exports: [MenubarModule, InputTextModule, TabViewModule, ButtonModule],
 })
 export class PrimengSharedModule {}
