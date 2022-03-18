da0c5e783637abddd2a8cbb9d16be7732d636342
Hazem <hazemzon@gmail.com>
2022-03-18 11:37:38 +0200

    fix responsive logo image & add responsive search bar

--- src/app/modules/cards/components/header/header-toolbar/header-toolbar.component.ts
+++ src/app/modules/cards/components/header/header-toolbar/header-toolbar.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderToolbarComponent implements OnInit {
   text!: string;
-
+  searchString!: string;
   results: string[] = [];
   constructor() {}
 
